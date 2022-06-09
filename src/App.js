acda005af83ff4dba32a0b73993a0d360c325c5b
Jhonatan Martinez <consejerojhon@gmail.com>
2022-06-08 20:12:57 -0400

    Added title and description strings.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let title_app = 'Curso Intensivo de React - Talendig';
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {title_app}
         </a>
       </header>
     </div>
