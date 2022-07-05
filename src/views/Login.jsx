e5c65f658d84ba89fde4a65e01fa775c0449b21b
Jhonatan Martinez <consejerojhon@gmail.com>
2022-07-04 20:42:09 -0400

    Added Axios to Login Component.

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,14 +1,49 @@
 import Nav from '../components/Nav';
+import axios from 'axios';
+import {encode, decode} from 'string-encode-decode';
 
-function Login(){
+function Login(props){
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        // let email = e.name.email;
+        // let password = encode(e.name.password);
+        // let password_remember = e.name.password_remember;
+        let email = document.getElementById('email').value;
+        let password = encode(document.getElementById('password').value);
+        let password_remember = document.getElementById('password_remember').value;
+
+        if(password_remember){
+            sessionStorage.setItem('email', email);
+            sessionStorage.setItem('password', password);
+        }
+
+        axios.post('http://localhost:8080/user/run/session', {
+            params: {
+                email: email,
+                password: password
+            }
+        }).then(response=>{
+            console.log('response', response);
+        }).catch((error)=>{
+            console.log('error', error);
+        });
+    }
+
     return (
         <div>
             <Nav />
             <div className="d-flex flex-row flex-wrap justify-content-center mt-5 w-100">
                 <h3 className="w-100 text-center">Iniciar sesión</h3>
-                <form action="" className="card p-3 w-100" style={{maxWidth: 350}}>
-                    <input type="email" placeholder="Correo electrónico" className="" required />
-                    <input type="password" placeholder="Contraseña" className="mt-3" required />
+                <form onSubmit={handleSubmit} className="card p-3 w-100" style={{maxWidth: 350}}>
+                    <input name="email" id="email" type="email" placeholder="Correo electrónico" className="" required />
+                    <input name="password" id="password" type="password" placeholder="Contraseña" className="mt-3" required />
+                    <div className="d-flex flex-row align-items-center p-2">
+                        <label htmlFor="password_remember" >Recordar credenciales:</label>
+                        <input id="password_remember" className="m-2" name="password_remember" type="checkbox" checked />
+                    </div>
+
                     <button type="submit" className="mt-3 text-primary">Iniciar sesión</button>
                 </form>
             </div>
