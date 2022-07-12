94a9efeef3313a73f2aecb92d2efee7e06fff7e5
Jhonatan Martinez <consejerojhon@gmail.com>
2022-07-12 19:13:55 -0400

    Added configuration Axios to Login Component.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ function Nav(){
     return (
         <div className="d-flex flex-row justify-content-between align-items-center flex-wrap w-100" style={{border: '1px solid black'}}>
             <span className="p-3 text-primary">
-                <Link to="/" class="text-primary">Project Learn</Link>
+                <Link to="/" className="text-primary">Project Learn</Link>
             </span>
             <div className="pt-3 pb-3 pl-5 pr-5 d-flex flex-row justify-content-center align-items-center" style={{minWidth: 290, width: "30%"}}>
                 <Link to="/login">Iniciar sesión</Link>
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,17 +1,19 @@
 import Nav from '../components/Nav';
 import axios from 'axios';
 import {encode, decode} from 'string-encode-decode';
+import {isDisabled} from "@testing-library/user-event/dist/utils";
+import {useState} from "react";
 
 function Login(props){
+
+    const [state, setEnable] = useState(1);
 
     function handleSubmit(e){
         e.preventDefault();
 
-        // let email = e.name.email;
-        // let password = encode(e.name.password);
-        // let password_remember = e.name.password_remember;
         let email = document.getElementById('email').value;
-        let password = encode(document.getElementById('password').value);
+        // let password = encode(document.getElementById('password').value);
+        let password = document.getElementById('password').value;
         let password_remember = document.getElementById('password_remember').value;
 
         if(password_remember){
@@ -19,7 +21,7 @@ function Login(props){
             sessionStorage.setItem('password', password);
         }
 
-        axios.post('http://localhost:8080/user/run/session', {
+        axios.post(process.env.REACT_APP_ROUTE_BACK + '/login', {
             params: {
                 email: email,
                 password: password
@@ -39,12 +41,13 @@ function Login(props){
                 <form onSubmit={handleSubmit} className="card p-3 w-100" style={{maxWidth: 350}}>
                     <input name="email" id="email" type="email" placeholder="Correo electrónico" className="" required />
                     <input name="password" id="password" type="password" placeholder="Contraseña" className="mt-3" required />
+
                     <div className="d-flex flex-row align-items-center p-2">
                         <label htmlFor="password_remember" >Recordar credenciales:</label>
-                        <input id="password_remember" className="m-2" name="password_remember" type="checkbox" checked />
+                        <input id="password_remember" className="m-2" name="password_remember" type="checkbox" />
                     </div>
 
-                    <button type="submit" className="mt-3 text-primary">Iniciar sesión</button>
+                    <button type="submit" className="mt-3 text-primary" disabled={!state}>Iniciar sesión</button>
                 </form>
             </div>
 
